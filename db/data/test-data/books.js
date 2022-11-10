a2719dcd87fc087cf264363299cd560c7431ee50
RossKeen <rosssamuelkeen@gmail.com>
2022-11-10 07:26:05 +0000

    add test and dev data

---
+++ db/data/test-data/books.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    title: "Mordew",
+    author: "Alex Pheby",
+    category: "Book",
+    status: "Reading",
+    finished: false,
+    created_at: new Date(1668010003),
+  },
+  {
+    title: "Moby Dick",
+    author: "Herman Melville",
+    category: "Book",
+    status: "Read",
+    finished: true,
+    created_at: new Date(1668010530),
+  },
+  {
+    title: "The Picture of Dorian Gray",
+    author: "Oscar Wilde",
+    category: "Book",
+    status: "Lost interest",
+    finished: true,
+    created_at: new Date(1668010656),
+  },
+  {
+    title: "The Silmarillion",
+    author: "J.R.R. Tolkien",
+    category: "Book",
+    status: "Not read",
+    finished: false,
+    created_at: new Date(1668010660),
+  },
+];
