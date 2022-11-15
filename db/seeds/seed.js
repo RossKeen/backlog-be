246d1e1f2cbae8658a3aaf290669a6b741fc6f81
RossKeen <rosssamuelkeen@gmail.com>
2022-11-15 14:26:34 +0000

    refactor data to be entries in a single table

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,64 +2,29 @@ const format = require("pg-format");
 const db = require("../connection.js");
 
 const seed = async (data) => {
-  const { gamesData, filmsData, booksData } = data;
+  const { entriesData } = data;
 
-  await db.query(`DROP TABLE IF EXISTS games`);
-  await db.query(`DROP TABLE IF EXISTS films`);
-  await db.query(`DROP TABLE IF EXISTS books`);
+  await db.query(`DROP TABLE IF EXISTS entries`);
 
   await db.query(`
-  CREATE TABLE games (
-    game_id SERIAL PRIMARY KEY,
-    game_title VARCHAR NOT NULL,
-    category VARCHAR DEFAULT 'Game' NOT NULL,
-    status VARCHAR DEFAULT 'Not played' NOT NULL,
+  CREATE TABLE entries (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    artist VARCHAR DEFAULT 'Unknown' NOT NULL,
+    category VARCHAR NOT NULL,
+    status VARCHAR DEFAULT 'Never' NOT NULL,
     finished BOOL DEFAULT false NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
   );`);
 
-  await db.query(`
-  CREATE TABLE films (
-    film_id SERIAL PRIMARY KEY,
-    film_title VARCHAR NOT NULL,
-    director VARCHAR DEFAULT 'Unknown',
-    category VARCHAR DEFAULT 'Film' NOT NULL,
-    status VARCHAR DEFAULT 'Not watched' NOT NULL,
-    finished BOOL DEFAULT false NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-  );`);
-
-  await db.query(`
-  CREATE TABLE books (
-    book_id SERIAL PRIMARY KEY,
-    book_title VARCHAR NOT NULL,
-    author VARCHAR DEFAULT 'Unknown',
-    category VARCHAR DEFAULT 'Book',
-    status VARCHAR DEFAULT 'Not read' NOT NULL,
-    finished BOOL DEFAULT false NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-  );`);
-
-  const insertGamesQueryStr = format(
-    "INSERT INTO games (game_title, category, status, finished, created_at) VALUES %L RETURNING *;",
-    gamesData.map(({ title, category, status, finished, created_at }) => [title, category, status, finished, created_at])
+  const insertEntriesQueryStr = format(
+    "INSERT INTO entries (title, artist, category, status, finished, created_at) VALUES %L RETURNING *;",
+    entriesData.map(({ title, artist, category, status, finished, created_at }) => [title, artist, category, status, finished, created_at])
   );
 
-  const insertFilmsQueryStr = format(
-    "INSERT INTO films (film_title, director, category, status, finished, created_at) VALUES %L RETURNING *;",
-    filmsData.map(({ title, director, category, status, finished, created_at }) => [title, director, category, status, finished, created_at])
-  );
+  const entriesPromise = db.query(insertEntriesQueryStr).then((result) => result.rows);
 
-  const insertBooksQueryStr = format(
-    "INSERT INTO books (book_title, author, category, status, finished, created_at) VALUES %L RETURNING *;",
-    filmsData.map(({ title, author, category, status, finished, created_at }) => [title, author, category, status, finished, created_at])
-  );
-
-  const gamesPromise = db.query(insertGamesQueryStr).then((result) => result.rows);
-  const filmsPromise = db.query(insertFilmsQueryStr).then((result) => result.rows);
-  const booksPromise = db.query(insertBooksQueryStr).then((result) => result.rows);
-
-  await Promise.all([gamesPromise, filmsPromise, booksPromise]);
+  await entriesPromise;
 };
 
 module.exports = seed;
